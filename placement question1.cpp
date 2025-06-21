66ac6b64d926158e930c723889c2c44a0937105f
Shreya Kushwaha <kushwahashreya39@gmail.com>
2025-06-21 14:30:45 +0530

    Delete placement question1.cpp

--- placement question1.cpp
+++
@@ -1,9 +0,0 @@
-//Given an integer array num[] sorted in non decreasing order remove the duplicates in place such that each unique elemnets appear only once.
-//Written the no of unique element in num array.
-//The relative order of the element should be kept same.
-//Placement question
-
-
-
-	
-
